82f03a7c7ac2a2caaff497759627471f077bd4e0
mighil31 <mighildath@gmail.com>
2023-02-02 21:52:40 +0530

    Initial commit

---
+++ src/main/java/com/roomy/roomy/RoomyApplication.java
@@ -0,0 +1,13 @@
+package com.roomy.roomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roomy/roomy/RoomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomy.roomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
